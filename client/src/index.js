a04e446d6522dca9593d9f7b0b25bd6b3ffea6fc
aaru01 <aartimedhe16601@gmail.com>
2023-12-04 17:01:00 +0530

    routing setup in client

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Home from "./views/Home/Home";
+import Home from "./views/Home/Home.js";
+import About from "./views/About/About.js";
+import Login from "./views/Login/Login.js"
+import Signup from "./views/Signup/Signup.js"
+import Services from "./views/Services/Services.js"
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/services",
+    element: <Services />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <Home />
+    <RouterProvider router={router} />
   </>
 );
