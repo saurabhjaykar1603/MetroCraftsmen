7b70793e50c946c65658a5dca4081bb9177b1f73
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-12-04 15:48:13 +0530

    style: add Bootstrap styles and JavaScript

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./views/Home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
