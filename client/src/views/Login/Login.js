72894c10dc76434a979afd2e8859e5bc2ef4f542
datta88 <dattatupe77@gmail.com>
2023-12-05 11:22:16 +0530

    added data successfull Signup  page in client

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -6,16 +6,20 @@ function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const loginButton = async () =>{
-    
-    const response = await axios.post('/api/login',{
+  const loginButton = async () => {
+    if (!email) {
+      return alert('Email is required')
+    }
+    if (!password) {
+      return alert('Password is required')
+    }
+
+    const response = await axios.post('/api/login', {
       email,
       password
     });
-    
-    console.log(response?.data?.message);
 
-    if(response?.data?.success){
+    if (response?.data?.success) {
       alert(response?.data?.message);
       window.location.href = '/'
     }
@@ -23,10 +27,10 @@ function Login() {
   return (
     <div>
       <h1>Login</h1>
-      
-    <input type='email' value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder='Email' /> 
-    <input type='password' value={password}  onChange={(e)=>{setPassword(e.target.value)}} placeholder='Password' />
-    <button type='button' onClick={loginButton}>Login</button>
+
+      <input type='email' value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder='Email' />
+      <input type='password' value={password} onChange={(e) => { setPassword(e.target.value) }} placeholder='Password' />
+      <button type='button' onClick={loginButton}>Login</button>
 
     </div>
   )
--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -1,12 +1,63 @@
 import React, { useState } from "react";
+import axios from 'axios'
 import "./Signup.css";
 
 function Signup() {
-  
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [address, setAddress] = useState('');
+  const [gender, setGender] = useState('male');
+
+
+  const signupButton = async () => {
+
+    if (!name) {
+      return alert('Name is required')
+    }
+    if (!email) {
+      return alert('Email is required')
+    }
+    if (!password) {
+      return alert('Password is required')
+    }
+    if (!address) {
+      return alert('Address is required')
+    }
+    if (!gender) {
+      return alert('Gender is required')
+    }
+    const response = await axios.post('/api/signup', {
+      name,
+      email,
+      password,
+      address,
+      gender,
+    })
+
+    if (response?.data?.success) {
+      alert(response?.data?.message);
+      window.location.href = '/login';
+    }
+
+  }
+
+
   return (
     <div>
       <h1>signup</h1>
-     
+      <input type="text" placeholder="Name" value={name} onChange={(e) => { setName(e.target.value) }} className="font" />
+      <input type="email" placeholder="Email" value={email} onChange={(e) => { setEmail(e.target.value) }} className="font" />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => { setPassword(e.target.value) }} className="font" />
+      <input type="text" placeholder="Address" value={address} onChange={(e) => { setAddress(e.target.value) }} className="font" />
+      <div>
+        <p className="font">male</p>
+        <input type="radio" name="gender" onChange={(e) => { setGender(e.target.value) }} value='male' checked='male' />
+        <p className="font">female</p>
+        <input type="radio" name="gender" onChange={(e) => { setGender(e.target.value) }} value='female' />
+      </div>
+
+      <button type="button" onClick={signupButton}>Signup</button>
     </div>
   );
 }
