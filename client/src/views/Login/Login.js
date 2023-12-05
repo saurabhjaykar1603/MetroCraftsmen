e57fd735136548332e23e65d1e3599b4f59ba887
datta88 <dattatupe77@gmail.com>
2023-12-05 10:53:19 +0530

    added data successfull  Login page in client

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 import "./Login.css"
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginButton = async () =>{
+    
+    const response = await axios.post('/api/login',{
+      email,
+      password
+    });
+    
+    console.log(response?.data?.message);
+
+    if(response?.data?.success){
+      alert(response?.data?.message);
+      window.location.href = '/'
+    }
+  }
   return (
     <div>
       <h1>Login</h1>
+      
+    <input type='email' value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder='Email' /> 
+    <input type='password' value={password}  onChange={(e)=>{setPassword(e.target.value)}} placeholder='Password' />
+    <button type='button' onClick={loginButton}>Login</button>
+
     </div>
   )
 }
--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Signup.css";
 
 function Signup() {
+  
   return (
     <div>
       <h1>signup</h1>
+     
     </div>
   );
 }
