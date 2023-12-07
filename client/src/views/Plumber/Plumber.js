ee22b5af0a61047342599655d3607ba12fddbc10
shitalwarkhade89 <shitalwarkhade89@gmail.com>
2023-12-07 20:35:25 +0530

    feat: integreate GET API of plumber

--- client/src/views/Plumber/Plumber.js
+++ client/src/views/Plumber/Plumber.js
@@ -1,12 +1,55 @@
-import React from 'react'
-import "./Plumber.css"
+import React,{useState,useEffect} from 'react'
+import "./Plumber.css";
+import axios from 'axios';
 import Navbar from '../../components/Navbar/Navbar'
+import Card from '../../components/Card/Card';
+import Footer from '../../components/Footer/Footer';
 
 function Plumber() {
+  const [plumbers, setPlumber] = useState();
+
+  const loadPlumber = async () => {
+    try {
+      const response = await axios.get("/api/v1/plumbers");
+      setPlumber(response?.data?.data);
+    }
+    catch (err) {
+      console.log(err);
+      alert("Error loading Plumber");
+    }
+
+  };
+
+  useEffect(() => {
+    loadPlumber();
+  }, [])
   return (
     <div>
       <Navbar/>
-      <h1>Plumber</h1>
+      <div className='container'>
+        <img src='https://res.cloudinary.com/urbanclap/image/upload/t_high_res_category/w_873,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/supply/customer-app-supply/1699522519517-6d1e4c.jpeg' alt='plumber image' className='plumber-image'/>
+      </div>
+     <div className='plumber-container'>
+     {
+          plumbers?.map((plumber, index) => {
+
+            const { name, contact, description, price, img } = plumber;
+
+            return (<Card
+              key={index}
+              name={name}
+              contact={contact}
+              description={description}
+              price={price}
+              img={img}
+            />);
+
+
+          })
+        }
+
+     </div>
+     <Footer/>
     </div>
   )
 }
