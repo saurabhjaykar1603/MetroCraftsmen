979558589fbbce8a0358e723e014cbc51bfbefae
datta88 <dattatupe77@gmail.com>
2023-12-07 20:58:20 +0530

    added about page shital all information

--- client/src/views/About/About.js
+++ client/src/views/About/About.js
@@ -2,7 +2,10 @@ import React from 'react'
 import "./About.css"
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
-import DattaImg from './Image/datta.jpg'
+import DattaImg from './Image/datta.jpg';
+import Aarti from './Image/aarti .jpeg';
+import Saurabh from './Image/saurabh.jpeg';
+import Shital from './Image/shital.jpeg';
 
 function About() {
   return (
@@ -11,59 +14,58 @@ function About() {
       <div>
         <div className='about-container-wonner fist-con'>
           <div className='img-container'>
-            <img src={DattaImg} className='img-about' />
+            <img src={} className='img-about' />
           </div>
           <div className='information-container '>
-            <p className='about-paraghrap'><span className='about-span-con'>Name  :-</span> Tupe Datta</p>
-            <p className='about-paraghrap'><span className='about-span-con'>Email :-</span>dattatupe77@gmail.com</p>
-            <p className='about-paraghrap'><span className='about-span-con'>GitHub :-</span> https://github.com/datta88</p>
-            <p className='about-paraghrap'><span className='about-span-con'>Linkdin :-</span> https://www.linkedin.com/in/dattatray-tupe/</p>
-            <p className='about-paraghrap'><span className='about-span-con'>PeerList :-</span> https://peerlist.io/dattatraytupe</p>
+            <p className='about-paraghrap'><span className='about-span-con'>Name :-</span> </p>
+            <p className='about-paraghrap'><span className='about-span-con'>Email :-</span></p>
+            <p className='about-paraghrap'><span className='about-span-con'>GitHub :-</span></p>
+            <p className='about-paraghrap'><span className='about-span-con'>Linkdin :-</span></p>
+            <p className='about-paraghrap'><span className='about-span-con'>PeerList :-</span> </p>
           </div>
         </div>
 
         <div className='about-container-wonner second'>
           <div className='img-container'>
-            <img src={DattaImg} className='img-about' />
+            <img src={Saurabh} className='img-about' />
           </div>
           <div className='information-container'>
-            <p className='about-paraghrap'><span className='about-span-con'>Name  :-</span> Tupe Datta</p>
-            <p className='about-paraghrap'><span className='about-span-con'>Email :-</span>dattatupe77@gmail.com</p>
-            <p className='about-paraghrap'><span className='about-span-con'>GitHub :-</span> https://github.com/datta88</p>
-            <p className='about-paraghrap'><span className='about-span-con'>Linkdin :-</span> https://www.linkedin.com/in/dattatray-tupe/</p>
-            <p className='about-paraghrap'><span className='about-span-con'>PeerList :-</span> https://peerlist.io/dattatraytupe</p>
+            <p className='about-paraghrap'><span className='about-span-con'>Name :-</span></p>
+            <p className='about-paraghrap'><span className='about-span-con'>Email :-</span></p>
+            <p className='about-paraghrap'><span className='about-span-con'>GitHub :-</span></p>
+            <p className='about-paraghrap'><span className='about-span-con'>Linkdin :-</span></p>
+            <p className='about-paraghrap'><span className='about-span-con'>PeerList :-</span></p>
           </div>
         </div>
 
         <div className='about-container-wonner fist-con'>
           <div className='img-container'>
-            <img src={DattaImg} className='img-about' />
+            <img src={Shital} className='img-about' />
           </div>
 
           <div className='information-container '>
-            <p className='about-paraghrap'><span className='about-span-con'>Name  :-</span> Tupe Datta</p>
-            <p className='about-paraghrap'><span className='about-span-con'>Email :-</span>dattatupe77@gmail.com</p>
-            <p className='about-paraghrap'><span className='about-span-con'>GitHub :-</span> https://github.com/datta88</p>
-            <p className='about-paraghrap'><span className='about-span-con'>Linkdin :-</span> https://www.linkedin.com/in/dattatray-tupe/</p>
-            <p className='about-paraghrap'><span className='about-span-con'>PeerList :-</span> https://peerlist.io/dattatraytupe</p>
+            <p className='about-paraghrap'><span className='about-span-con'>Name  :-</span>Warkhade Shital</p>
+            <p className='about-paraghrap'><span className='about-span-con'>Email :-</span>shitalwarkhade89@gmail.com</p>
+            <p className='about-paraghrap'><span className='about-span-con'>GitHub :-</span> https://github.com/shitalwarkhade89</p>
+            <p className='about-paraghrap'><span className='about-span-con'>Linkdin :-</span>  https://www.linkedin.com/in/<br/>shitalwarkhade89/</p>
+            <p className='about-paraghrap'><span className='about-span-con'>PeerList :-</span>  https://peerlist.io/shitalwarkhade</p>
           </div>
         </div>
 
         <div className='about-container-wonner second'>
           <div className='img-container'>
-            <img src={DattaImg} className='img-about' />
+            <img src={} className='img-about' />
           </div>
           <div className='information-container'>
-            <p className='about-paraghrap'><span className='about-span-con'>Name  :-</span> Tupe Datta</p>
-            <p className='about-paraghrap'><span className='about-span-con'>Email :-</span>dattatupe77@gmail.com</p>
-            <p className='about-paraghrap'><span className='about-span-con'>GitHub :-</span> https://github.com/datta88</p>
-            <p className='about-paraghrap'><span className='about-span-con'>Linkdin :-</span> https://www.linkedin.com/in/dattatray-tupe/</p>
-            <p className='about-paraghrap'><span className='about-span-con'>PeerList :-</span> https://peerlist.io/dattatraytupe</p>
+            <p className='about-paraghrap'><span className='about-span-con'>Name  :-</span> </p>
+            <p className='about-paraghrap'><span className='about-span-con'>Email :-</span></p>
+            <p className='about-paraghrap'><span className='about-span-con'>GitHub :-</span></p>
+            <p className='about-paraghrap'><span className='about-span-con'>Linkdin :-</span></p>
+            <p className='about-paraghrap'><span className='about-span-con'>PeerList :-</span></p>
           </div>
         </div>
 
       </div>
-      <h1>About</h1>
       <Footer />
     </div>
   )
