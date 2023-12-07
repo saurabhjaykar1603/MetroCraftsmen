7ce09ac532e32cc80d0ea29372b79d2302495101
datta88 <dattatupe77@gmail.com>
2023-12-07 19:28:52 +0530

    added about second  page

--- client/src/views/About/About.js
+++ client/src/views/About/About.js
@@ -22,9 +22,22 @@ function About() {
           </div>
         </div>
 
+        <div className='about-container-wonner second'>
+          <div className='img-container'>
+            <img src={DattaImg} className='img-about' />
+          </div>
+          <div className='information-container'>
+            <p className='about-paraghrap'><span className='about-span-con'>Name  :-</span> Tupe Datta</p>
+            <p className='about-paraghrap'><span className='about-span-con'>Email :-</span>dattatupe77@gmail.com</p>
+            <p className='about-paraghrap'><span className='about-span-con'>GitHub :-</span> https://github.com/datta88</p>
+            <p className='about-paraghrap'><span className='about-span-con'>Linkdin :-</span> https://www.linkedin.com/in/dattatray-tupe/</p>
+            <p className='about-paraghrap'><span className='about-span-con'>PeerList :-</span> https://peerlist.io/dattatraytupe</p>
+          </div>
+        </div>
+
         
       </div>
-      
+      <h1>About</h1>
       <Footer />
     </div>
   )
