f5fa93c5c5669aa88b655251a5c3cc7ac8ba3fe7
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-12-15 10:57:07 +0530

    feat (api) : api endpoint created for post services

---
+++ server/controllers/servicename.js
@@ -0,0 +1,30 @@
+import ServiceName from "../models/ServiceName.js";
+
+const postServices = async (req, res) => {
+  const { serviceName, contact, speciality, img, price, name } = req.body;
+  const Service = new ServiceName({
+    serviceName,
+    name,
+    contact,
+    speciality,
+    img,
+    price,
+  });
+  try {
+    const savedService = await Service.save();
+    res.status(201).json({
+      success: true,
+      data: savedService,
+      message: "Create Plumber Successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+
+
+export { postServices, getServicesByName };
